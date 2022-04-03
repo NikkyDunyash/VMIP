8db836835af9ff07115a4e841ec11c7bf2803951
Nikky <simnick108@mail.ru>
2022-04-03 16:51:48 +0300

    unified regularization

--- deconvolution/main.py
+++ deconvolution/main.py
@@ -1,11 +1,12 @@
 import argparse
-from tabnanny import verbose
 import skimage.io as io
 import numpy as np
 from functional import *
 import optim
-import time
-from tqdm import tqdm
+# from tqdm import tqdm
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import sklearn.linear_model
 
 
 def create_parser():
@@ -19,6 +20,13 @@ def create_parser():
 def normalize(image):
     return (image-image.min())/(image.max()-image.min())
 
+def gen_alpha1(noise_level):
+    return 0.0094*noise_level+0.04
+
+def gen_alpha2(noise_level):
+    return 0.0004*noise_level+0.018
+
+
 def main():
     namespace=create_parser().parse_args()
     input_image=255*normalize(
@@ -29,16 +37,36 @@ def main():
     N=200
     residual_norm=ResL1(A=Conv(kernel), b=input_image)
     directions=[[0 , 1], [1, 0], [1, 1], [1, -1]]
-    alpha1, alpha2=(0.3, 0.01)
-    func=residual_norm+alpha1*TV(directions) #+alpha2*TV2(directions)
+
+    # best alphas were found for 0, 5, 10, 15, 20 noise levels
+    # and linearly approximated
+
+    # noise_lvls=np.arange(25, step=5)
+    # alpha1=[0.02, 0.09, 0.17, 0.18, 0.21]  # best alphas found
+    # alpha2=[0.01, 0.03, 0.02, 0.03, 0.02]  # best alphas found
+    # plt.scatter(noise_lvls, alpha1, label=r'$\alpha_1$', marker='+')
+    # plt.scatter(noise_lvls, alpha2, label=r'$\alpha_2$', marker='+')
+    # lin_reg=sklearn.linear_model.LinearRegression().fit(noise_lvls.reshape(-1, 1), alpha1)
+    # print(lin_reg.coef_, lin_reg.intercept_)
+    # lin_reg.fit(noise_lvls.reshape(-1, 1), alpha2)
+    # print(lin_reg.coef_, lin_reg.intercept_)
+    # plt.plot(np.arange(21), gen_alpha1(np.arange(21)))
+    # plt.plot(np.arange(21), gen_alpha2(np.arange(21)))
+    # plt.legend()
+    # plt.show()
+    
+    # print(gen_alpha1(namespace.noise_level), gen_alpha2(namespace.noise_level))
+
+    func=residual_norm + 0.3*TV(directions)\
+        # +gen_alpha1(namespace.noise_level)*TV(directions) \
+        # +gen_alpha2(namespace.noise_level)*TV2(directions) 
     optimizer=optim.GD(func, x0=np.zeros(input_image.shape), lr=1, 
-        momentum_factor=0.9, nesterov=False)
+        momentum_factor=0.9, nesterov=True)
     scheduler=optim.ReduceLROnPlateau(optimizer, patience=1, verbose=True)
-    start_time=time.perf_counter()
-    for i in tqdm(range(N)):
+    # for i in tqdm(range(N)):
+    for k in range(N):
         scheduler.step()
-    end_time=time.perf_counter()
-    print(f'{N} calls:', end_time-start_time, 'secs')
+    
     output_image=255*normalize(optimizer.x[ :, :, np.newaxis])
     output_image=np.array(np.repeat(output_image, repeats=3, axis=2), dtype=np.uint8)
     io.imsave(namespace.output_image, output_image)
